cb560534e68a42bd0ceb65fd741802d90b2cd771
Sean Huber <shuber@huberry.com>
2011-06-07 21:39:42 -0700

    refactor the constants initializer

--- initializers/constants.php
+++ initializers/constants.php
@@ -1,13 +1,28 @@
 <?php
 
 namespace Phuby {
-    const DS = DIRECTORY_SEPARATOR;
-    const NS = '\\';
-    const PS = PATH_SEPARATOR;
-    const VERSION = '0.0.0';
-
-    // [QUIRK] Cannot use "const FOO = 'BAR'" syntax with string concatenation
-    //         e.g. "const __NS__ = __NAMESPACE__.NS;"
+    /**
+     * [QUIRK] Cannot use "const FOO = 'BAR'" syntax with string concatenation or functions
+     *         e.g. "const ROOT = dirname(__DIR__).DIRECTORY_SEPARATOR.'lib';"
+     *
+     * [QUIRK] Namespaces are downcased in get_defined_constants() keys when constants are
+     *         defined with the "const" keyword
+    **/
+    define(__NAMESPACE__.'\\NS',      '\\');
     define(__NAMESPACE__.NS.'__NS__', __NAMESPACE__.NS);
-    define(__NS__.'ROOT', dirname(__DIR__).DS.'lib');
+    define(__NS__.'DS',               DIRECTORY_SEPARATOR);
+    define(__NS__.'PS',               PATH_SEPARATOR);
+    define(__NS__.'ROOT',             dirname(__DIR__).DS.'lib');
+    define(__NS__.'VERSION',          '0.0.0');
+
+    /**
+     * [QUIRK] Constants defined in the Phuby namespace are not resolved when called from subnamespaces
+     *         so we need to manually define them for use in the Phuby\Core namespace
+    **/
+    $constants = get_defined_constants(true);
+    foreach ($constants['user'] as $name => $value) {
+        if (preg_match('#^'.preg_quote(__NS__).'(.+)$#', $name, $matches)) {
+            define(__NS__.'Core'.NS.$matches[1], $value);
+        }
+    }
 }
