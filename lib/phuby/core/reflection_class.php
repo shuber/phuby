1d1987faa9c5fa22b335e6b07b43178f8c316565
Sean Huber <shuber@huberry.com>
2011-06-09 14:51:45 -0700

    update Core\ReflectionClass

--- lib/phuby/core/reflection_class.php
+++ lib/phuby/core/reflection_class.php
@@ -3,6 +3,10 @@
 namespace Phuby\Core {
     class ReflectionClass extends \ReflectionClass {
 
+        const INSTANCE_METHOD = 'instance';
+        const STATIC_METHOD = 'static';
+
+        protected $_methods;
         protected $_parent;
 
         static protected $_instances = array();
@@ -12,8 +16,39 @@ namespace Phuby\Core {
             $this->_parent = get_parent_class($class);
         }
 
+        function getInstanceMethods() {
+            return $this->getCachedMethods(self::INSTANCE_METHOD);
+        }
+
         function getParentClass() {
             if ($this->_parent) return static::instance($this->_parent);
+        }
+
+        function getStaticMethods() {
+            return $this->getCachedMethods(self::STATIC_METHOD);
+        }
+
+        function lookupMethod($method, $method_type) {
+            $methods = $this->getCachedMethods($method_type);
+            return isset($methods[$method]) ? $methods[$method] : false;
+        }
+
+        protected function getCachedMethods($method_type = null) {
+            if (!in_array($method_type, array(null, self::INSTANCE_METHOD, self::STATIC_METHOD))) {
+                throw new \InvalidArgumentException('Invalid method type "'.$method_type.'"');
+            } else if (!isset($this->_methods)) {
+                $this->_methods = array(
+                    self::INSTANCE_METHOD => array(),
+                    self::STATIC_METHOD => array()
+                );
+                foreach ($this->getMethods() as $method) {
+                    if ($method->getDeclaringClass()->name == $this->name) {
+                        $type = $method->isStatic() ? self::STATIC_METHOD : self::INSTANCE_METHOD;
+                        $this->_methods[$type][$method->name] = $method;
+                    }
+                }
+            }
+            return is_null($method_type) ? $this->_methods : $this->_methods[$method_type];
         }
 
         static function instance($class) {
--- test/unit/core/reflection_class_test.php
+++ test/unit/core/reflection_class_test.php
@@ -1,21 +1,65 @@
 <?php
 
 namespace Phuby\Core\ReflectionClassTest {
-    class User extends \Phuby\Object { }
+    class User extends \Phuby\Object {
+        function instance_method() { }
+        static function static_method() { }
+    }
+
+    class Admin extends User { }
 }
 
 namespace Phuby\Core {
     class ReflectionClassTest extends \ztest\UnitTestCase {
 
         function setup() {
-            $this->reflection = new ReflectionClass(__CLASS__.NS.'User');
+            $this->user_reflection = new ReflectionClass(__CLASS__.NS.'User');
+            $this->admin_reflection = new ReflectionClass(__CLASS__.NS.'Admin');
+        }
+
+        function test_getInstanceMethods() {
+            assert_equal(array('instance_method'), array_keys($this->user_reflection->getInstanceMethods()));
+            assert_equal(array(), $this->admin_reflection->getInstanceMethods());
         }
 
         function test_getParentClass() {
-            assert_identical(ReflectionClass::instance(__NS__.'Object'), $this->reflection->getParentClass());
+            assert_identical(ReflectionClass::instance(__NS__.'Object'), $this->user_reflection->getParentClass());
 
             $reflection = new ReflectionClass(__NS__.'Object');
             assert_null($reflection->getParentClass());
+        }
+
+        function test_getStaticMethods() {
+            assert_equal(array('static_method'), array_keys($this->user_reflection->getStaticMethods()));
+            assert_equal(array(), $this->admin_reflection->getStaticMethods());
+        }
+
+        function test_lookupMethod() {
+            assert_equal('instance_method', $this->user_reflection->lookupMethod('instance_method', ReflectionClass::INSTANCE_METHOD)->name);
+            assert_equal(false, $this->user_reflection->lookupMethod('invalid', ReflectionClass::INSTANCE_METHOD));
+
+            assert_equal('static_method', $this->user_reflection->lookupMethod('static_method', ReflectionClass::STATIC_METHOD)->name);
+            assert_equal(false, $this->user_reflection->lookupMethod('invalid', ReflectionClass::STATIC_METHOD));
+
+            assert_equal(false, $this->admin_reflection->lookupMethod('instance_method', ReflectionClass::INSTANCE_METHOD));
+            assert_equal(false, $this->admin_reflection->lookupMethod('static_method', ReflectionClass::STATIC_METHOD));
+        }
+
+        function test_protected_getCachedMethods() {
+            $method = new \ReflectionMethod(__CORE__.'ReflectionClass', 'getCachedMethods');
+            $method->setAccessible(true);
+            $reflection = $this->user_reflection;
+
+            $methods = $method->invoke($reflection);
+            assert_array($methods);
+            ensure(array_key_exists(ReflectionClass::INSTANCE_METHOD, $methods));
+            ensure(array_key_exists(ReflectionClass::STATIC_METHOD, $methods));
+            ensure(is_a(reset(reset($methods)), 'ReflectionMethod'));
+
+            assert_equal($methods[ReflectionClass::INSTANCE_METHOD], $method->invoke($reflection, ReflectionClass::INSTANCE_METHOD));
+            assert_equal($methods[ReflectionClass::STATIC_METHOD], $method->invoke($reflection, ReflectionClass::STATIC_METHOD));
+
+            assert_throws('InvalidArgumentException', function() use ($method, $reflection) { $method->invoke($reflection, 'invalid'); });
         }
 
         function test_static_instance() {
