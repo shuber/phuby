c567a319e0bf9c7e5abc42f89e1e181c662e59c9
Sean Huber <shuber@huberry.com>
2011-07-27 19:18:36 -0700

    don't silently fail if initializers fail to load

--- phuby.php
+++ phuby.php
@@ -2,5 +2,5 @@
 
 namespace Phuby {
     $initializers = glob(__DIR__.DIRECTORY_SEPARATOR.'initializers'.DIRECTORY_SEPARATOR.'*');
-    if ($initializers) foreach ($initializers as $initializer) require $initializer;
+    foreach ($initializers as $initializer) require $initializer;
 }
