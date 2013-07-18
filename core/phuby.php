6775c4b5e7de5ae1ba68a4637738604ac8c3e997
Sean Huber <sean.m.huber@gmail.com>
2013-07-18 15:44:16 -0700

    Allow Phuby function to accept any argument

--- core/phuby.php
+++ core/phuby.php
@@ -1,8 +1,22 @@
 <?php
 
-function Phuby($object) {
-    if (is_string($object) && class_exists($object))
+function Phuby($object, $lookup_class = true) {
+    if ($lookup_class && is_string($object) && class_exists($object))
         return Phuby\Module::const_get($object);
+
+    // if (is_numeric_array($object))
+    //     return Phuby\Array::__new($object);
+
+    if (is_array($object))
+        return Phuby\Hash::__new($object);
+
+    switch(gettype($object)) {
+        case 'float':   return Phuby\Float::__new($object);
+        case 'integer': return Phuby\Integer::__new($object);
+        case 'object':  return Phuby\Proxy::__new($object);
+        case 'string':  return Phuby\String::__new($object);
+        default:        return $object;
+    }
 }
 
 trait Phuby {
