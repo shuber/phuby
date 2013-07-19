8492e001ea627116748ada78a95e478a36976aee
Sean Huber <sean.m.huber@gmail.com>
2013-07-19 13:42:28 -0700

    Base64 is already a module so we don't need a separate InstanceMethods module

--- lib/phuby/base64.php
+++ lib/phuby/base64.php
@@ -1,22 +1,17 @@
 <?php
 
-namespace Phuby {
-    class Base64 {
-        static function initialized($self) {
-            $self->extend('self');
-            $self->include(__CLASS__.'\InstanceMethods');
-        }
+namespace Phuby;
+
+class Base64 {
+    static function initialized($self) {
+        $self->extend('self');
     }
-}
 
-namespace Phuby\Base64 {
-    class InstanceMethods {
-        function decode64($string) {
-            return base64_decode($string);
-        }
+    function decode64($string) {
+        return base64_decode($string);
+    }
 
-        function encode64($string) {
-            return base64_encode($string);
-        }
+    function encode64($string) {
+        return base64_encode($string);
     }
 }
