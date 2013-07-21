43e5606dced29a145ba1254a1d6732233671b626
Sean Huber <sean.m.huber@gmail.com>
2013-07-21 16:00:22 -0700

    Rename ArrayAccess method names

--- core/phuby/traits/array_access.php
+++ core/phuby/traits/array_access.php
@@ -4,18 +4,18 @@ namespace Phuby\Traits;
 
 trait ArrayAccess {
     function offsetExists($offset) {
-        return $this->__send__('array_access_exists', $offset);
+        return $this->__send__('array_access_offset_exists', $offset);
     }
 
     function offsetGet($offset) {
-        return $this->__send__('array_access_get', $offset);
+        return $this->__send__('array_access_offset_get', $offset);
     }
 
     function offsetSet($offset, $value) {
-        return $this->__send__('array_access_set', $offset, $value);
+        return $this->__send__('array_access_offset_set', $offset, $value);
     }
 
     function offsetUnset($offset) {
-        return $this->__send__('array_access_unset', $offset);
+        return $this->__send__('array_access_offset_unset', $offset);
     }
 }
