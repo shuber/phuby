afe4b774601238f74571a465a51b8fcf4cfebb67
Sean Huber <sean.m.huber@gmail.com>
2013-07-23 00:39:44 -0700

    Move all bootstrapping logic into bootstrap.php

--- phuby.php
+++ phuby.php
@@ -1,14 +1,3 @@
 <?php
 
 require_once 'lib/bootstrap.php';
-
-trait Phuby {
-    use Phuby\Core;
-
-    static function initialized($self) {
-        while ($superclass = $self->superclass())
-            $self = $superclass;
-
-        $self->__include(__TRAIT__.'\Object');
-    }
-}
