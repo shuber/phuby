3fbf02703e44a26026c9996b00d74f18cf00b45e
Sean Huber <sean.m.huber@gmail.com>
2013-07-23 16:12:23 -0700

    Add BasicObject#equal? method

--- lib/phuby/basic_object.php
+++ lib/phuby/basic_object.php
@@ -1,7 +1,19 @@
 <?php
 
-namespace Phuby;
+namespace Phuby {
+    class BasicObject {
+        use Core;
+    }
+}
 
-class BasicObject {
-    use Core;
+namespace Phuby\BasicObject {
+    class InstanceMethods {
+        static function initialized($self) {
+            $self->alias_method('equal?', 'equal_query');
+        }
+
+        function equal_query($object) {
+            return $this == $object;
+        }
+    }
 }
