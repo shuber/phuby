5bad03e20edefb51b21c84b34bea6602b566120d
Sean Huber <sean.m.huber@gmail.com>
2013-08-07 23:04:55 -0700

    Enumerable.numeric returns false of $object is not an array

--- lib/phuby/enumerable/class_methods.php
+++ lib/phuby/enumerable/class_methods.php
@@ -4,6 +4,9 @@ namespace Phuby\Enumerable;
 
 class ClassMethods {
     function numeric($object) {
+        if (!is_array($object))
+            return false;
+
         foreach ($object as $key => $value)
             if (!is_int($key))
                 return false;
