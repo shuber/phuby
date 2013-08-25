73afda910b7256924748ad1ab75757e952931173
Sean Huber <sean.m.huber@gmail.com>
2013-08-25 00:00:30 -0700

    Add CoreTest

---
+++ test/CoreTest.php
@@ -0,0 +1,17 @@
+<?php
+
+class CoreTest extends PHPUnit_Framework_TestCase {
+    /**
+     * @expectedException Phuby\NoMethodError
+     */
+    function testSendWithInvalidMethodName() {
+        $object = new Phuby\Object;
+        $object->__send__('invalid');
+    }
+
+    function testDefineMethod() {
+        $object = new Phuby\Object;
+        $object->singleton_class->define_method('testing', function() { return 'test'; });
+        $this->assertEquals('test', $object->testing);
+    }
+}
