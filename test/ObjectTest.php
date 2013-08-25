a2ce2a757c158d4cf0aeaa8da38e8117182fe40b
Sean Huber <sean.m.huber@gmail.com>
2013-08-24 23:48:16 -0700

    Add tests for Object

--- test/ObjectTest.php
+++ test/ObjectTest.php
@@ -1,7 +1,17 @@
 <?php
 
-class ObjectTest extends PHPUnit_Framework_TestCase {
-    function testItWorks() {
-        $this->assertEquals(true, true);
+namespace Phuby;
+
+class ObjectTest extends \PHPUnit_Framework_TestCase {
+    function setUp() {
+        $this->Object = Phuby(__NAMESPACE__.'\Object');
+    }
+
+    function test_it_should_inherit_basic_object() {
+        $this->assertContains(__NAMESPACE__.'\BasicObject', $this->Object->heritage());
+    }
+
+    function test_it_should_include_kernel() {
+        $this->assertContains(__NAMESPACE__.'\Kernel', $this->Object->ancestors());
     }
 }
