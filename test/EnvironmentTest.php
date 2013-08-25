a4d4a55857ba4a44c88b8da66dee5faa184816ba
Sean Huber <sean.m.huber@gmail.com>
2013-08-25 00:18:48 -0700

    Use phpunit's dataProvider annotation

--- test/EnvironmentTest.php
+++ test/EnvironmentTest.php
@@ -1,18 +1,22 @@
 <?php
 
 class EnvironmentTest extends PHPUnit_Framework_TestCase {
-    function testFilename() {
-        $classes = [
-            'User' => 'user.php',
-            'TestIng' => 'test_ing.php',
-            '\TestIng' => 'test_ing.php',
-            '\Namespaced\Test\Ing' => 'namespaced/test/ing.php',
-            'Namespaced\Test\Ing' => 'namespaced/test/ing.php',
-            'TestIng_Underscore' => 'test_ing_underscore.php'
+    /**
+     * @dataProvider files
+     */
+    function test_filename($class, $filename) {
+        $result = Phuby\Environment::filename($class);
+        $this->assertEquals($filename, $result);
+    }
+
+    function files() {
+        return [
+            ['User','user.php'],
+            ['TestIng', 'test_ing.php'],
+            ['\TestIng', 'test_ing.php'],
+            ['\Namespaced\Test\Ing', 'namespaced/test/ing.php'],
+            ['Namespaced\Test\Ing', 'namespaced/test/ing.php'],
+            ['TestIng_Underscore', 'test_ing_underscore.php']
         ];
-        foreach ($classes as $class => $expected) {
-            $result = Phuby\Environment::filename($class);
-            $this->assertEquals($expected, $result);
-        }
     }
 }
